4dc69a0d4f7281c5b26545fd4e624de287fe35d1
orchsik <orchsik@naver.com>
2022-03-31 13:52:47 +0900

    router

--- lib/src/pages/normal/first.dart
+++ lib/src/pages/normal/first.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/src/pages/normal/second.dart';
 
 class FirstPage extends StatelessWidget {
   const FirstPage({Key? key}) : super(key: key);
@@ -8,6 +10,19 @@ class FirstPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("First Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text("다음페이지 이동"),
+              onPressed: () {
+                Get.to(SecondPage());
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/src/pages/normal/second.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/src/home.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text("뒤로 이동"),
+              onPressed: () {
+                // [old]
+                // Navigator.of(context).pop();
+                Get.back();
+              },
+            ),
+            ElevatedButton(
+              child: Text("홈 이동"),
+              onPressed: () {
+                // Get.to(Home());
+                Get.offAll(Home());
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
